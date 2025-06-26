d16bfee5ea97e4fe911ba07ad78b97be9fb1b449
srkdesign <99723068+srkdesign@users.noreply.github.com>
2025-06-26 20:35:14 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import os
+from PIL import Image, ImageDraw, ImageFont
+
+def add_watermarks(input_folder="images", output_folder="watermarked", text="© srkdesign"):
+    os.makedirs(output_folder, exist_ok=True)
+
+    font_path = "fonts/PPNeueMontreal-Medium.otf"
+    font_size = 72
+    font = ImageFont.truetype(font_path, font_size)
+
+    ascent, descent = font.getmetrics()
+    text_height = ascent + descent
+
+    opacity = 120
+
+    for filename in os.listdir(input_folder):
+        image_path = os.path.join(input_folder, filename)
+        output_path = os.path.join(output_folder, filename)
+
+        image = Image.open(image_path).convert("RGBA")
+        watermark = Image.new("RGBA", image.size, (0,0,0,0))
+        
+        draw = ImageDraw.Draw(image)
+        text_width = draw.textlength(text, font=font)
+
+        text_image = Image.new("RGBA", (int(text_width), int(text_height)), (0, 0, 0, 0))
+        text_draw = ImageDraw.Draw(text_image)
+        text_draw.text((0,0), text, font=font, fill=(255, 255, 255, opacity))
+
+        rotated = text_image.rotate(45, expand=1)
+
+        step_x = rotated.width + 50
+        step_y = rotated.height + 100
+
+        margin_x = 25
+        margin_y = -75
+
+        for y in range(-image.height + margin_y, image.height * 2, step_y):
+            for x in range(-image.width + margin_x, image.width * 2, step_x):
+                watermark.paste(rotated, (x, y), rotated)
+
+        combined = Image.alpha_composite(image, watermark)
+        combined.convert("RGB").save(output_path, "JPEG")
+
+        print(f"Saved watermarked image: {filename}")
+
+
+if __name__ == "__main__":
+    add_watermarks()
